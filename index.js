5245d658583e15ebef36e390fb08dcc9e398e6c5
Andry Pebrianto <andrypeb227@gmail.com>
2023-08-24 16:49:25 +0700

    Membuat endpoint /, /testimonial, dan /contact pada express server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const path = require("path");
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+// home page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// testimonial page
+app.get("/testimonial", (req, res) => {
+  res.render("testimonial");
+});
+// contact page
+app.get("/contact-me", (req, res) => {
+  res.render("contact");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
